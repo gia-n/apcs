408be7e748680cb78a3231579c9d743fe98ca3c4
gia-n <gznichoson@gmail.com>
2017-09-07 10:52:25 -0700

    Add files via upload

---
+++ APCS/Flag/ReadMe2.java
@@ -0,0 +1,8 @@
+/*initiaizing project with a read me
+ * project will be split into three parts
+ * creating the red and white stripes (complete)
+ * next will be to create the blue union background (complete)
+ * third will be to to create and organise the white stars (comlete)
+ * EXTRA!! i added color to the stars which can easily be removed and just have white stars
+ * (idk if you want an american pride flag but it was fun)
+ */
---
+++ APCS/Flag/main.java
@@ -0,0 +1,8 @@
+//to run flag project
+public class main {
+public static void main(String[] args) {
+flag c = new flag();
+c.setDefaultCloseOperation(c.EXIT_ON_CLOSE);
+c.setVisible(true);
+}
+}
